f24c08ce6058437f74d71dc5e9142c9b9c422a18
Fvamesm <warmhhands@gmail.com>
2018-10-21 17:45:42 +0800

    线程状态 线程中断恢复

---
+++ demo/src/main/java/james/multithread/threadState/ThreadInterruptDemo.java
@@ -0,0 +1,49 @@
+package james.multithread.threadState;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by James on 2018/10/21.
+ */
+public class ThreadInterruptDemo {
+
+    private static int i = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        //interruptMethod();
+
+        recoverInterruptStateMethod();
+    }
+
+    private static void recoverInterruptStateMethod() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                boolean state = Thread.currentThread().isInterrupted();
+                if (state) {
+                    System.out.println("before :" + state);
+                    Thread.interrupted(); // 对线程复位，中断标识为 false
+                    System.out.println("after: " + Thread.currentThread().isInterrupted());
+                }
+            }
+        });
+
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        thread.interrupt(); // 设置中断标识
+    }
+
+    private static void interruptMethod() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            // 没有中断指令时继续加
+            while (!Thread.currentThread().isInterrupted()) {
+                i++;
+                System.out.println("STATTE: " + Thread.currentThread().getState());
+            }
+            System.out.println("num: " + i);
+        }, "interruptDemo");
+
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        thread.interrupt();
+    }
+}
