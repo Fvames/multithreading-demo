0bd885163177d2777aa6bfa4716374566d9ca59b
Fvamesm <warmhhands@gmail.com>
2018-10-26 22:53:24 +0800

    volitor 进一步理解注释

--- demo/src/main/java/james/multithread/sharDataInThread/VolatileDemo.java
+++ demo/src/main/java/james/multithread/sharDataInThread/VolatileDemo.java
@@ -4,7 +4,8 @@ package james.multithread.sharDataInThread;
  * Created by James on 2018/10/19.
  */
 public class VolatileDemo implements Runnable{
-    // 仅能保证线程间数据共享，但不能保证原子性、有序性，需要使用 synchronized
+    // 仅能保证线程间数据共享，但不能保证原子性，需要使用 synchronized，
+    //      有序性：volatile 修饰的变量之前的代码不会再变量之后执行，但是之前的代码涉及到： 读，计算，回写；这个过程是不安全的，如果其他cpu进入就没有办法保证原子性，而 synchronized 关键字编译后会使用 jvm高级指令的 monitorenter 和 monitorexit 包裹代码块，保证原子性
     // 当前线程的修改立即同步到主内存，但其他线程依然需要从主线程中获取数据，时间差
     private volatile int anInt;
 
